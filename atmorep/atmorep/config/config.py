9a828eb382431fdcff3d53fdb54cdaecad527c4c
sascholle <sascholle@uni-osnabrueck.de>
2025-01-21 15:45:15 +0100

    New tree structure directory organisation

---
+++ atmorep/atmorep/config/config.py
@@ -0,0 +1,15 @@
+import os 
+from pathlib import Path
+
+fpath = os.path.dirname(os.path.realpath(__file__))
+
+path_models = Path( fpath, '../../models/')
+path_results = Path( fpath, '../../results')
+path_plots = Path( fpath, '../results/plots/')
+
+grib_index = { 'vorticity' : 'vo', 'divergence' : 'd', 'geopotential' : 'z',
+                'orography' : 'z', 'temperature': 't', 'specific_humidity' : 'q',
+                'mean_top_net_long_wave_radiation_flux' : 'mtnlwrf',
+                'velocity_u' : 'u', 'velocity_v': 'v', 'velocity_z' : 'w',
+                'total_precip' : 'tp', 'radar_precip' : 'yw_hourly',
+                't2m' : 't_2m', 'u_10m' : 'u_10m', 'v_10m' : 'v_10m',  }
